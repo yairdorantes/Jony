615b5244e006b89c22d6bd619b8249a2a182c318
yair <yairmasterlol@gmail.com>
2024-01-09 17:45:02 -0600

    lol

--- api/views.py
+++ api/views.py
@@ -2,44 +2,23 @@
 from django.views import View
 import json
 from django.http import JsonResponse
-
-import cv2
-import numpy as np
 from pyzbar.pyzbar import decode
 import base64
+import io
+
+from PIL import Image
 
 
 def BarcodeReader(base64_image):
-    # Decode the base64 string to bytes
     image_data = base64.b64decode(base64_image)
-
-    # Convert the bytes to a NumPy array
-    nparr = np.frombuffer(image_data, np.uint8)
-
-    # Decode the NumPy array to an OpenCV image
-    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-
-    # The rest of your barcode decoding logic remains unchanged
-    detectedBarcodes = decode(img)
-
-    if not detectedBarcodes:
+    pil_image = Image.open(io.BytesIO(image_data))
+    detected_barcodes = decode(pil_image)
+    if not detected_barcodes:
         print("Barcode Not Detected or your barcode is blank/corrupted!")
     else:
-        for barcode in detectedBarcodes:
-            (x, y, w, h) = barcode.rect
-            cv2.rectangle(
-                img, (x - 10, y - 10), (x + w + 10, y + h + 10), (255, 0, 0), 2
-            )
-
-            if barcode.data != "":
-                print(barcode.data)
-                # print(barcode.type)
-                return {"barcode_data": barcode.data.decode("utf-8")}
-                # return json.dumps({"barcode": barcode.data})
-
-    # cv2.imshow("Image", img)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
+        for barcode in detected_barcodes:
+            print(barcode.data)
+            return {"barcode_data": barcode.data.decode("utf-8")}
 
 
 # from django.contrib.auth import authenticate
